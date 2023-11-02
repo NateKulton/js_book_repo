05551094f20727b894df0309dbde44d8509ab874
Nate <natekulton@gmail.com>
2023-11-02 14:27:55 -0400

    update to school stuff

---
+++ the_basics/basicProblems.js
@@ -0,0 +1,50 @@
+// Problem 1:
+  let firstName = 'Nate ';
+  let lastName = 'Kulton';
+  let fullName = firstName + lastName;
+  console.log(fullName);
+
+//Problem 2:
+/*
+  I struggled on this one but I think I got it now.
+  I was trying to write in a .js file instead of Node REPL,
+  and it made it confusing because I didn't know the value of my variables as they were changing
+  but I'll just count this as done because I just followed along with the video to under the principals of the problem
+*/
+
+// Problem 3:
+/*
+  'true' = String
+  false = Boolean
+  1.5 = Number (or Float in other languages)
+  2 = Number
+  undefined = Undefined
+  { foo: 'bar' } Object (or key-value pairs in other languages)
+  // (I forgot to use on cap on "object" in my og answer)
+*/
+
+// Problem 4:
+
+// Because with concatenation JavaScript coerces the number into a string value before they are concatenated
+
+// Problem 5:
+  console.log(Number('5')+ 10);
+
+// Problem 6:
+  let num = Number('5') + 10;
+  console.log(`The value of 5 + 10 is ${num}.`);
+
+ // Problem 7:
+ // This was the answer, interseting return value
+ // JavaScript seems like it lets you make more mistakes than other languages do
+  let foo = ['a', 'b', 'c'];
+  console.log(foo.length);  // => 3
+  console.log(foo[3]);      // undefined
+
+ // Problem 8:
+  let names = ['asta', 'butterscotch', 'puddoing', 'neptune', 'darwin'];
+  console.log(names);
+
+  // Problem 9:
+
+
