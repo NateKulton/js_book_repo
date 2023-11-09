3606d2d47c075122cad86c9f997692a819471d1c
Nate <natekulton@gmail.com>
2023-11-09 16:17:40 -0500

    edited a js file

--- variables/age.js
+++ variables/age.js
@@ -1,9 +1,26 @@
 // Problem 1:
 
+/*
 let age = 20;
 console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age + 10} years old.`);
 console.log(`In 20 years, you will be ${age + 20} years old.`);
 console.log(`In 30 years, you will be ${age + 30} years old.`);
 console.log(`In 40 years, you will be ${age + 40} years old.`);
+*/
+
 // I forgot to write console.log(`You are ${age} years old.`); lol
+
+//input_output Problem:
+
+let rlySync = require('readline-sync');
+let age = Number(rlySync.question('How old are you? '))
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 20 years, you will be ${age + 20} years old.`);
+console.log(`In 30 years, you will be ${age + 30} years old.`);
+console.log(`In 40 years, you will be ${age + 40} years old.`);
+
+/*
+ So it looks like my answer was almost the same
+ */
