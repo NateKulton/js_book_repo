45f1a0c43239655368beae17c94ebead1a293ee0
Nate <natekulton@gmail.com>
2023-11-10 13:54:36 -0500

    misc file

---
+++ misc_problems/say2.js
@@ -0,0 +1,10 @@
+function say(text) {
+  console.log(text);
+}
+
+// text is just a placeholder parameter it could be anything we want it to be
+
+say("hello");
+say("hi");
+say("how do you do");
+say("Quite all right");
