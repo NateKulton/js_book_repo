641f5c7d52d5b809cd3c6d7a21a959266d19df4b
Nate <natekulton@gmail.com>
2023-11-15 17:10:18 -0500

    UPDATE

--- input_output/greeter.js
+++ input_output/greeter.js
@@ -1,18 +1,24 @@
 // Problem 1:
+
+// const { prompt } = require("readline-sync");
+
+/*
 let readlineSync = require('readline-sync');
 
 let name = readlineSync.question('What is your name? ');
 console.log(`Hello, ${name}!`);
+*/
 
 // Problem 2:
 
-
+/*
 let rlSync = require('readline-sync');
 
 let firstName = rlSync.question('What is your first name? ');
 let lastName = rlSync.question('What is your last name? ');
 let fullName = firstName + ' ' + lastName;
 console.log(`Hello, ${fullName}!`);
+*/
 
 /*
   so at first I used all the same values 'rlySync' because
@@ -24,3 +30,23 @@ console.log(`Hello, ${fullName}!`);
 // This is Browser Style
 // let name = prompt('What is your name? ');
 // console.log(`Hello, ${name}!`);
+
+
+
+
+//console.log(`Hello, ${fullName}!`);
+//let fullName = firstName + ' ' + lastName;
+
+// This is the edit from the function lesson
+
+
+function getName(prompt) {
+  let rlSync = require('readline-sync');
+  let name = rlSync.question(prompt);
+  return name;
+}
+
+let firstName = getName('What is your first name? ');
+let lastName = getName('What is your last name? ');
+let fullName = firstName + ' ' + lastName;
+console.log(`Hello, ${fullName}!`);
