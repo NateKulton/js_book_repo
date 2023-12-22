61f113662a7d07c734c1208754ce027600515031
Nate <natekulton@gmail.com>
2023-12-22 17:52:20 -0500

    Array chapter UPDATE

---
+++ arrays/arrayNotes.md
@@ -0,0 +1,49 @@
+Problem 1:
+my answers: array1: 1, array2: 5, array3: 1 (right answer 0), array4: 3, array5: 1 (right answer 101)
+
+Problem 2:
+
+I knew I needed to use the % operator but I forgot how to write the code properly
+
+Problem 3:
+
+my answer had the same output but is was less clean (I can't rememeber the actually term)
+
+Problem 4:
+
+So my answer was almost the same except I didn't use return statements,
+ and I used my console.log in the functiion instead. All in all, I'd say I was on track with this one
+
+Problem 5:
+
+I took my time on this one but I did it so well! The big differnce between mine the LS. School one
+is that I used a if when I could have just used the return statement without it because Number.isInteger
+returns a boolean value so if I just used that in the return statement it would have been like a ternary operator
+
+Problem 6:
+
+at one point I did make a function that gave me the answer but it didn't have both .map and .filter in it.
+Also this lesson was misleading when they say I have to use both of those but then never said a word about .length tbh
+I thought it was cheating to use it because they never said a word about it.
+So I tried this again and I think I found out the problem, I was using the not operator on the whole function
+so it never ran the code because it turned the function into a falsy value
+
+Problem 7:
+
+So I got the problem right... At least the output that is. I didn't use a single function, I used two of them.
+Plus I used the .forEach as well and the .reduce method in my code.
+I understand the big differnce between the two answers. I made a new array with the new value that
+I made from the og array, but they made a function that iterates over the array, and then they passed the array as
+an argument to the function in the console.log so it outputs the resolute of that code
+
+Problem 8:
+
+This was a very complicated problem so I was a little confused with it
+
+Problem 9:
+
+I was trying to make all this crazy code and it was just a simple method, oof
+
+Problem 10:
+
+This one was super easy! I just used the idex of the nested array then the index of the target element
--- arrays/arrayProblems.js
+++ arrays/arrayProblems.js
@@ -1,1 +1,176 @@
-testArray = ['Hello', 'World'];
+// Problem 3:
+
+/*
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+myArray[0].forEach(function(value) {
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+});
+
+myArray[1].forEach(function(value) {
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+});
+
+myArray[2].forEach(function(value) {
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+});
+*/
+
+// Problem 4:
+
+/*
+
+let myArray = [
+  1, 3, 6, 11,
+  4, 2, 4, 9,
+  17, 16, 0,
+];
+
+
+
+let secondArray = myArray.map(function(value) {
+  if (value % 2 === 0) {
+   console.log('Even'); // This should be this instead: return 'Even';
+  } else {
+   console.log('Odd'); // This should be this instead: return 'Odd';
+  }
+});
+
+*/
+
+// And the conesole.log should be out here to check the answer
+
+// Problem 5:
+
+/*
+
+let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+let integers = things.filter(function(value) {
+  if (Number.isInteger(value)) {
+    return value;
+  }
+});
+
+console.log(integers);
+
+*/
+
+/*
+
+let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+let integers = things.filter(function(value) {
+return Number.isInteger(value);
+});
+
+console.log(integers);
+
+*/
+
+// Problem 6:
+
+// LS: answer
+
+/*
+function oddLengths(strings) {
+  let lengths = strings.map((letters) => letters.length);
+  let oddLengths = lengths.filter((number) => number % 2 === 1); // I tried to write mine like this
+  return oddLengths;                                             // (!((number) => number % 2 === 1))
+}                                                                // It seems like JS doesn't treat this as a boolean
+                                                                 // Like I thought it did
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr));
+*/
+
+/*
+function oddLengths(strings) {
+  let lengths = strings.map((letters) => letters.length);
+  let oddLengths = lengths.filter((number) => number % 2 !== 0);
+  return oddLengths;
+}
+
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr));
+*/
+
+// Probleme 7
+
+/*
+let array = [3, 5, 7];
+let timesArray = [];
+array.forEach((times) => timesArray.push (times * times))
+let squaredNum = timesArray.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+console.log(squaredNum); // output => 83
+*/
+
+// LS School code:
+
+/*
+function sumOfSquares(numbers) {
+  return numbers.reduce((accumulator, number) => {
+    return accumulator + number * number;
+  }, 0);
+}
+
+let array = [3, 5, 7];
+console.log(sumOfSquares(array)); // => 83
+*/
+
+// Problem 8:
+
+//LS school answer:
+
+/*
+function oddLengths(strings) {
+  return strings.reduce((filteredNumbersArray, letters) => {
+    let length = letters.length;
+    if (length % 2 === 1) {
+      filteredNumbersArray.push(length);
+    }
+
+    return filteredNumbersArray;
+  }, []);
+}
+
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr));
+*/
+
+// Problem 9:
+
+
+// LS answer:
+
+/*
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];
+
+console.log(numbers1.includes(3));
+
+console.log(numbers2.includes(3));
+
+console.log(numbers3.includes(3));
+*/
+
+// Problem 10:
+
+let arr = [
+  ["hello", "world"],
+  ["example", "mem", null, 6, 88],
+  [4, 8, 12]
+];
+
+arr[1][3] = 606;
+
+console.log(arr);
